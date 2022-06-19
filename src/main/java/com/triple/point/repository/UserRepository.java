80d9c83f9b02e834ebe80e19699ff306523d4e39
khj <zla1275@gamil.com>
2022-06-19 15:26:44 +0900

    UserRepository.java: 인터페이스 추가 UserRepositoryImpl.java: 인터페이스를 구현하도록 수정

--- src/main/java/com/triple/point/repository/UserRepository.java
+++ src/main/java/com/triple/point/repository/UserRepository.java
@@ -1,23 +1,10 @@
 package com.triple.point.repository;
 
 import com.triple.point.domain.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
 import java.util.UUID;
 
-@Repository
-@RequiredArgsConstructor
-public class UserRepository {
-
-    private final EntityManager em;
-
-    public void registryUser(User user) {
-        em.persist(user);
-    }
-
-    public User getUser(UUID userId) {
-        return em.find(User.class, userId);
-    }
+public interface UserRepository {
+    public void registryUser(User user);
+    public User getUser(UUID userId);
 }
