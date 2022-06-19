c41ca413ae1f54dc7e9025bc201153d2bce129bf
khj <zla1275@gamil.com>
2022-06-20 00:40:31 +0900

    BasicPointPolicy.java: 내용 포인트, 사진 첨부 포인트 계산루틴 추가 CalcPointDTO.java: reviewId 제거, placeId 추가

--- src/main/java/com/triple/point/domain/dto/CalcPointDTO.java
+++ src/main/java/com/triple/point/domain/dto/CalcPointDTO.java
@@ -1,17 +1,18 @@
 package com.triple.point.domain.dto;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 public class CalcPointDTO {
 
-    private String reviewId;
-    private String content;
-    private String[] attachedPhotoIds;
+    private final String content;
+    private final String placeId;
+    private final String[] attachedPhotoIds;
 
     public CalcPointDTO(EventDTO eventDTO){
-        this.reviewId = eventDTO.getReviewId();
         this.content = eventDTO.getContent();
+        this.placeId = eventDTO.getPlaceId();
         this.attachedPhotoIds = eventDTO.getAttachedPhotoIds();
     }
 }
--- src/main/java/com/triple/point/service/BasicPointPolicy.java
+++ src/main/java/com/triple/point/service/BasicPointPolicy.java
@@ -5,6 +5,25 @@ import com.triple.point.domain.dto.CalcPointDTO;
 public class BasicPointPolicy implements PointPolicy {
     @Override
     public int calculatePoint(CalcPointDTO calcPointDTO) {
+        int result = 0;
+
+        result += contentPoint(calcPointDTO.getContent());
+        result += photoPoint(calcPointDTO.getAttachedPhotoIds());
+        result += bonusPoint(calcPointDTO.getPlaceId());
+
+        return result;
+    }
+
+    private int bonusPoint(String placeId) {
+        // TODO 보너스 포인트 계산
         return 0;
+    }
+
+    private int photoPoint(String[] attachedPhotoIds) {
+        return attachedPhotoIds.length > 0 ? 1 : 0;
+    }
+
+    private int contentPoint(String content) {
+        return content.length() > 0 ? 1 : 0;
     }
 }
