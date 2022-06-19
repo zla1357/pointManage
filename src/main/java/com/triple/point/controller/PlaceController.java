91f583a676dadd26702acb31861be3816f7abdb0
khj <zla1275@gamil.com>
2022-06-19 23:15:37 +0900

    기존 로직 제거

--- src/main/java/com/triple/point/controller/PlaceController.java
+++
@@ -1,9 +0,0 @@
-package com.triple.point.controller;
-
-import com.triple.point.domain.dto.PlaceDTO;
-
-public interface PlaceController {
-
-    public String registryPlace(String name);
-    public PlaceDTO getPlace(String placeId);
-}
--- src/main/java/com/triple/point/controller/UserController.java
+++
@@ -1,8 +0,0 @@
-package com.triple.point.controller;
-
-import com.triple.point.domain.dto.UserDTO;
-
-public interface UserController {
-    public String registryUser(String name);
-    public UserDTO getUser(String userId);
-}
